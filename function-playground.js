c4a2e4cf8521644887a6a7a926ad80641be7a261
Jaemin Han <j4emin.han@gmail.com>
2021-03-19 05:51:32 +0000

    Commitin

---
+++ function-playground.js
@@ -0,0 +1,8 @@
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
---
+++ loopylighthouse.js
@@ -0,0 +1,16 @@
+for(let i = 100; i <= 200; i++){
+  if(i % 3 === 0){
+    if(i % 4 === 0){
+      console.log("LoopyLighthouse")
+    }
+    else{
+      console.log("Loopy")
+    }
+  }
+  else if(i % 4 === 0){
+    console.log("Lighthouse")
+  }
+  else{
+    console.log(i)
+  }
+}
